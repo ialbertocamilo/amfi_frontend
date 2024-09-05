20e7442d7b5de068688a88d4dad32aaa2acf1215
Alberto Camilo Rodriguez Vizcarra <svg.z32@gmail.com>
2024-09-05 11:13:06 -0500

    feat add upload.sh

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:20-alpine AS build
+
+
+WORKDIR /web
+ENV NEXT_PUBLIC_SOCKET_URL="http://0.0.0.0:3000"
+COPY package*.json ./
+RUN yarn
+COPY . .
+RUN yarn run build
+
+FROM node:20-alpine AS runtime
+LABEL authors="Alberto Camilo Rodriguez Vizcarra"
+
+WORKDIR /web
+COPY package*.json ./
+COPY --from=build /web/.next ./.next
+COPY --from=build /web/public ./public
+COPY --from=build /web/node_modules ./node_modules
+EXPOSE 3000
+CMD ["yarn", "start"]
---
+++ upload.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+APP_NAME="amfi_frontend"
+VERSION="latest"
+DOCKER_USERNAME="isvgxd"
+
+echo $DOCKER_USERNAME
+echo "$DOCKER_PASSWORD"
+
+
+echo "$DOCKER_USERNAME/$APP_NAME:latest"
+echo "Iniciando sesión en Docker Hub..."
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+
+echo "Construyendo la imagen Docker..."
+docker build -t "$APP_NAME:$VERSION" .
+
+echo "Etiquetando la imagen..."
+docker tag "$APP_NAME:$VERSION" "$DOCKER_USERNAME/$APP_NAME:$VERSION"
+
+echo "Subiendo la imagen a Docker Hub..."
+docker push "$DOCKER_USERNAME/$APP_NAME:latest"
+
+echo "¡Imagen subida con éxito!"
