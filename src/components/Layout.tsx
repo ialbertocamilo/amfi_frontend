3d10e977e2d7ac543bfc8a898e607b3c96530895
Alberto Camilo Rodriguez Vizcarra <svg.z32@gmail.com>
2024-11-14 02:34:18 -0500

    feat: cicd and fix layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
 import '../app/globals.css';
+import Navbar from "./Navbar";
 import RecoilContextProvider from "./RecoilContextProvider";
+import Sidebar from "./Sidebar";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -29,35 +29,31 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
     handleResize();
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
   }, []);
 
   return (
-      <div className="flex h-screen bg-gray-100">
-  <div
-        className={`fixed inset-y-0 z-50 left-0 w-64 transition-transform transform shadow-lg ${
-          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        } md:relative md:translate-x-0 md:block`}
+    <div className="flex h-screen bg-gray-100">
+      <div
+        className={`fixed inset-y-0 z-50 left-0 w-64 transition-transform transform shadow-lg ${isSidebarOpen ? "translate-x-0 relative" : "-translate-x-full"
+          }`}
       >
-        {isSidebarOpen && <Sidebar />}
-  </div>
-        <div className="flex-1 flex flex-col overflow-auto">
-          <div className="flex items-center justify-between p-4 bg-white shadow-md">
-            <button
-                className="p-2 focus:outline-none focus:bg-gray-200 z-40"
-                onClick={toggleSidebar}
-            >
-              <FaBars className="w-6 h-6" />
-            </button>
-            <Navbar />
-          </div>
-          <main className="flex-1 p-6 overflow-auto">
-            <div className="bg-white min-h-screen p-6">  <RecoilContextProvider>{children}</RecoilContextProvider></div>
-          </main>
-        </div>
+        <Sidebar />
       </div>
+      <div className="flex-1 flex flex-col overflow-auto">
+        <div className="flex items-center justify-between p-4 bg-white shadow-md">
+          <button
+            className="p-2 focus:outline-none focus:bg-gray-200 z-40"
+            onClick={()=>toggleSidebar()}
+          >
+            <FaBars className="w-6 h-6" />
+          </button>
+          <Navbar />
+        </div>
+        <main className="flex-1 p-6 overflow-auto">
+          <div className="bg-white min-h-screen p-6">  <RecoilContextProvider>{children}</RecoilContextProvider></div>
+        </main>
+      </div>
+    </div>
   );
 };
 
