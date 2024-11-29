e64c2f3854a8584b8982fa794d9a71ebac68fd6a
Alberto Camilo Rodriguez <proveedor152@culqi.com>
2024-11-28 22:43:40 -0500

    fix: add loader to lista proyectod admin

--- src/pages/lista-de-proyectos-admin.tsx
+++ src/pages/lista-de-proyectos-admin.tsx
@@ -4,12 +4,12 @@ import {useRouter} from "next/router";
 import toast from "react-hot-toast";
 import PaginatedComponent from "@/components/PaginationComponent";
 import {getProjects} from "@/api/projectApi";
-import moment from "moment";
 import Layout from "@/components/Layout";
 import useUser from "@/hooks/user.hook";
 import ActionProjects from "@/components/ActionProjects";
 import {ProjectMapper} from "@/mappers/project.mapper";
 import {formatToLocalTime} from "@/lib/utils";
+import Loader from "@/components/Loader";
 
 const ListaProyectosAdmin = () => {
     const headers = [
@@ -33,6 +33,7 @@ const ListaProyectosAdmin = () => {
 
     const fetchProjects = useCallback(async () => {
         try {
+            setLoader(true)
             const response = await getProjects();
             const projectsData = response?.map((proyecto: any, index: number) => ({
                 correlativo: index + 1,
@@ -40,9 +41,9 @@ const ListaProyectosAdmin = () => {
                 proyecto: proyecto.name,
                 agencia: proyecto.agency?.name,
                 anunciante: proyecto.advertiser?.name ?? '-',
-                fechaRegistro:  formatToLocalTime(proyecto.createdAt),
+                fechaRegistro: formatToLocalTime(proyecto.createdAt),
                 estado: ProjectMapper.mapProjectStatus(proyecto.status),
-                creador: proyecto.creator?.name + ' ' + (proyecto?.creator?.lastName||''),
+                creador: proyecto.creator?.name + ' ' + (proyecto?.creator?.lastName || ''),
                 action: (
                     <ActionProjects
                         id={proyecto.id}
@@ -52,13 +53,15 @@ const ListaProyectosAdmin = () => {
             }));
             setProjects(projectsData);
         } catch (error: any) {
-            console.error("Error fetching projects:", error);
             if (error.status === 400) {
                 error.response?.data?.message.forEach((value: any) => toast.error(value));
             }
             if (error.status === 409) {
                 toast.error(error.response?.data?.clientMessage);
             }
+        }finally {
+
+            setLoader(false)
         }
     }, [user?.role]);
 
@@ -68,39 +71,42 @@ const ListaProyectosAdmin = () => {
         }
     }, [loading, fetchProjects]);
 
+    const [loader, setLoader] = useState(true)
     return (
         <Layout>
-            <div className="flex justify-between items-center mb-4">
-                <h1 className="text-2xl font-semibold">Lista de proyectos</h1>
-            </div>
+            <Loader loading={loader}>
+                <div className="flex justify-between items-center mb-4">
+                    <h1 className="text-2xl font-semibold">Lista de proyectos</h1>
+                </div>
 
-            <div className="flex flex-col md:flex-row mb-4 justify-between">
-                <div className="flex w-full md:w-1/4 mb-2 md:mb-0">
-                    <input
-                        type="text"
-                        placeholder="Filtrar tabla..."
-                        className="p-2 border border-gray-300 rounded w-full"
-                    />
-                    <button className="ml-2 bg-red-500 text-white py-2 px-4 rounded">
-                        Ver
+                <div className="flex flex-col md:flex-row mb-4 justify-between">
+                    <div className="flex w-full md:w-1/4 mb-2 md:mb-0">
+                        <input
+                            type="text"
+                            placeholder="Filtrar tabla..."
+                            className="p-2 border border-gray-300 rounded w-full"
+                        />
+                        <button className="ml-2 bg-red-500 text-white py-2 px-4 rounded">
+                            Ver
+                        </button>
+                    </div>
+                    <button
+                        className="bg-red-500 text-white py-2 px-4 rounded"
+                        onClick={crearProyecto}
+                    >
+                        Nuevo proyecto
                     </button>
                 </div>
-                <button
-                    className="bg-red-500 text-white py-2 px-4 rounded"
-                    onClick={crearProyecto}
-                >
-                    Nuevo proyecto
-                </button>
-            </div>
 
-            <div className="bg-white shadow-md rounded">
-                <PaginatedComponent
-                    view={(id: string) => router.push(`/detalle-proyecto?id=${id}`)}
-                    headers={headers}
-                    items={projects}
-                    itemsPerPage={10}
-                />
-            </div>
+                <div className="bg-white shadow-md rounded">
+                    <PaginatedComponent
+                        view={(id: string) => router.push(`/detalle-proyecto?id=${id}`)}
+                        headers={headers}
+                        items={projects}
+                        itemsPerPage={10}
+                    />
+                </div>
+            </Loader>
         </Layout>
     );
 };
